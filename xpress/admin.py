41f50a38e3011e86c39c076bf54804b983c16557
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-06-10 11:46:23 +0000

    user Profile

--- xpress/admin.py
+++ xpress/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group,User
+from .models import Profile
 
 
 # Register your models here.
 admin.site.unregister(Group)
 
-
+class ProfileInline(admin.StackedInline):
+    model = Profile
 class userAdmin(admin.ModelAdmin):
     model = User
     fields = ['username']
+    inlines = [ProfileInline]
 
 #unregistering the user modle
 admin.site.unregister(User)
 
 admin.site.register(User,userAdmin)
+
+# admin.site.register(Profile)
+
+
--- xpress/models.py
+++ xpress/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     follows = models.ManyToManyField("self", related_name='followed_by', symmetrical=False,blank=True)
+
+    def __str__(self):
+        return self.user.username
