6031bd0fe3d569b59ec3cffab09ccb5bae43f448
samuel-kwame-gyamfi <samuelkwameowusugyamfi@gmail.com>
2025-06-10 11:30:18 +0000

    creating admin panel and userprofile model

--- xpress/admin.py
+++ xpress/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group,User
+
 
 # Register your models here.
+admin.site.unregister(Group)
+
+
+class userAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+
+#unregistering the user modle
+admin.site.unregister(User)
+
+admin.site.register(User,userAdmin)
